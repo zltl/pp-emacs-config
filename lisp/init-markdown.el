5a2c15d8b7d47abf7f1f1402a97fbfda51639894
liaotonglang <liaotonglang@gmail.com>
2021-12-16 10:33:18 +0800

    add markdown support

---
+++ lisp/init-markdown.el
@@ -0,0 +1,13 @@
+;;; init-markdown.el --- markdown config
+
+;;; Code:
+
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+(provide 'init-markdown)
+;;; init-markdown.el ends here
