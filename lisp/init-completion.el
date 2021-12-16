9ff0b082a292c3c7afa4a38b21a44ad610b0bc6c
liaotonglang <liaotonglang@gmail.com>
2021-12-16 13:11:33 +0800

    put yaspette into company's backend

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -10,6 +10,17 @@
   (yas-minor-mode . (lambda () (diminish 'yas-minor-mode))))
 (use-package yasnippet-snippets)
 
+;; Add yasnippet support for all company backends
+;; https://github.com/syl20bnr/spacemacs/pull/179
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+(defun company-mode/backend-with-yas (backend)
+  "If company-mode/enable-yas, put company-yasnippet into BACKEND."
+  (if (or (not company-mode/enable-yas)
+	  (and (listp backend) (member 'company-yasnippet backend)))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
 (use-package company
   :ensure t
   :diminish company-mode
@@ -19,7 +30,9 @@
         company-tooltip-align-annotations t
         company-tooltip-limit 20
         company-show-quick-access t
-        company-minimum-prefix-length 1))
+        company-minimum-prefix-length 1)
+  (setq company-backends
+	(mapcar #'company-mode/backend-with-yas company-backends)))
 
 (provide 'init-completion)
 ;;; init-completion.el ends here
--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -84,12 +84,6 @@
                   (concat auto-save-list-file-prefix "tramp-\\2") t)
             (list ".*" auto-save-list-file-prefix t)))
 
-;; Favor spaces over tabs. Pls dun h8, but I think spaces (and 4 of them) is a
-;; more consistent default than 8-space tabs. It can be changed on a per-mode
-;; basis anyway (and is, where tabs are the canonical style, like go-mode).
- (setq-default indent-tabs-mode nil
-              tab-width 4)
-
 ;; Only indent the line when at BOL or in a line's indentation. Anywhere else,
 ;; insert literal indentation.
 (setq-default tab-always-indent nil)
