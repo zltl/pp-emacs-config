9fa18935eeb28efe68c2ed2f46fbddbbe3f651cb
liaotonglang <liaotonglang@gmail.com>
2021-12-17 15:19:01 +0800

    save undo-tree cache file into ~/.emacs.d/cache/undo/
    keep workspace clean.

--- lisp/init-undo.el
+++ lisp/init-undo.el
@@ -1,20 +1,18 @@
 ;;; init-undo.el --- undo tree
-
+;;; Commentary:
 ;;; Code:
+
+;;(setq undo-tree-history-directory-alist '(("." . (expand-file-name "undo" user-emacs-directory))))
+
+(defvar my-undo-cache-dir (expand-file-name "undo" my-cache-dir)
+  "Undotree cache location.")
 
 (use-package undo-tree
   :diminish nil
   :config
-  (defun undo-tree-visualizer-update-linum (&rest args)
-    (linum-update undo-tree-visualizer-parent-buffer))
-  (advice-add 'undo-tree-visualize-undo :after #'undo-tree-visualizer-update-linum)
-  (advice-add 'undo-tree-visualize-redo :after #'undo-tree-visualizer-update-linum)
-  (advice-add 'undo-tree-visualize-undo-to-x :after #'undo-tree-visualizer-update-linum)
-  (advice-add 'undo-tree-visualize-redo-to-x :after #'undo-tree-visualizer-update-linum)
-  (advice-add 'undo-tree-visualizer-mouse-set :after #'undo-tree-visualizer-update-linum)
-  (advice-add 'undo-tree-visualizer-set :after #'undo-tree-visualizer-update-linum)
-  (global-undo-tree-mode)
-  (setq undo-tree-auto-save-history t))
+  (setq undo-tree-auto-save-history t)
+  (setq undo-tree-history-directory-alist
+	(cons (cons "." my-undo-cache-dir) ())))
 
 (provide 'init-undo)
 ;;; init-undo.el ends here
