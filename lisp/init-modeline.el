b6496960f47bb833b44d563e640fd29d00e5d8a5
liaotonglang <liaotonglang@gmail.com>
2021-12-28 10:44:30 +0800

    always start doom-modeline if 'all-the-icons' fonts install.

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -2,29 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun my/font-installed-p (font-name)
-  "Check if font with FONT-NAME is available."
-  (if (find-font (font-spec :name font-name))
-      t
-    nil))
+;;(defun my/font-installed-p (font-name)
+;;  "Check if font with FONT-NAME is available."
+;;  (if (find-font (font-spec :name font-name))
+;;      t
+;;    nil))
 
-(use-package all-the-icons
-;;  :config
-;; INSTAL FONT MANUAL!
-;;  (when (and (eq system-type 'gnu/linux)
-;;			 (not (my/font-installed-p "all-the-icons")))
-;; 	(all-the-icons-install-fonts t))
-  )
+;; call all-the-icons-install-fonts manual.
+(use-package all-the-icons)
 
-(defun my/start-doom-modeline-only-graphic ()
-  "Start doom-modeline only in graphic window."
-  (if (and (display-graphic-p)
-		   (my/font-installed-p "all-the-icons"))
-	  (doom-modeline-mode 1)))
+;;(defun my/start-doom-modeline-if-fonts-exists ()
+;;  "Start doom-modeline only in graphic window."
+;;  (if (my/font-installed-p "all-the-icons")
+;;	  (doom-modeline-mode 1)))
 
 (use-package doom-modeline
   :hook
-  (after-init . my/start-doom-modeline-only-graphic))
+  (after-init . doom-modeline-mode))
 
 (provide 'init-modeline)
 ;;; init-modeline.el ends here
