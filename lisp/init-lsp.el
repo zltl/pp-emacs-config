bccce2446c60328d8205744837bd1d34fccea737
liaotonglang <liaotonglang@gmail.com>
2021-12-28 11:26:30 +0800

    use ">" as lsp-header-array
    lsp-headerline-array may conflict with all-the-icons fonts, terminal cannot
    display all-the-icons. just define

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -30,25 +30,16 @@
         ("C-c u" . lsp-ui-imenu))
   :hook (lsp-mode . lsp-ui-mode)
   :config
-  (setq lsp-ui-sideline-enable t
-        lsp-ui-sideline-delay 0.2
-        lsp-ui-sideline-show-diagnostics t
-        lsp-ui-sideline-show-hover nil
-        lsp-ui-sideline-show-code-actions t
-        lsp-signature-auto-activate t
-        lsp-signature-render-documentation t
-        lsp-modeline-code-actions-enable t
-        lsp-enable-symbol-highlighting t
-        lsp-ui-sideline-update-mode t
-        lsp-ui-doc-enable t
-        lsp-ui-doc-position top
-        lsp-eldoc-enable-hover t
-        lsp-ui-imenu-auto-refresh t
-        lsp-ui-imenu-refresh-delay 1)
+  (defun custom-setting-after-start
+	  (setq lsp-ui-imenu-auto-refresh 'after-save
+			lsp-headerline-arrow ">"))
+  (add-hook 'lsp-ui-mode-hook 'custom-setting-after-start)
   (define-key lsp-ui-mode-map [remap xref-find-definitions]
     #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references]
     #'lsp-ui-peek-find-references))
+
+(setq lsp-headerline-arrow "/")
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -2,19 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
-;;(defun my/font-installed-p (font-name)
-;;  "Check if font with FONT-NAME is available."
-;;  (if (find-font (font-spec :name font-name))
-;;      t
-;;    nil))
-
-;; call all-the-icons-install-fonts manual.
+;; NOTE: call all-the-icons-install-fonts manual.
 (use-package all-the-icons)
-
-;;(defun my/start-doom-modeline-if-fonts-exists ()
-;;  "Start doom-modeline only in graphic window."
-;;  (if (my/font-installed-p "all-the-icons")
-;;	  (doom-modeline-mode 1)))
 
 (use-package doom-modeline
   :hook
