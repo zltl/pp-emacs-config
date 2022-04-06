3418cf68c36eb274be2d394b2c60bdda94738bf0
liaotonglang <liaotonglang@gmail.com>
2022-04-06 12:01:22 +0800

    not call set-fontset-font if not exists

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -9,19 +9,22 @@
 
 (when (eq system-type 'darwin)
   (setq fonts '("SF Mono" "冬青黑体简体中文"))
-  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
+  (when (fboundp 'set-fontset-font)
+	(set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
   (set-face-attribute 'default nil :font
                       (format "%s:pixelsize=%d" (car fonts) 14)))
 
 (when (eq system-type 'windows-nt)
   (setq fonts '("Source Code Pro" "思源黑体"))
-  (set-fontset-font t 'unicode "Segoe UI Emoji" nil 'prepend)
+  (when (fboundp 'set-fontset-font)
+	(set-fontset-font t 'unicode "Segoe UI Emoji" nil 'prepend))
   (set-face-attribute 'default nil :font
                       (format "%s:pixelsize=%d" (car fonts) 20)))
 
 (when (eq system-type 'gnu/linux)
   (setq fonts '("Source Code Pro" "思源黑体"))
-  (set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend)
+  (when (fboundp 'set-fontset-font)  
+	(set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend))
   (set-face-attribute 'default nil :font
                       (format "%s:pixelsize=%d" (car fonts) 20)))
 
