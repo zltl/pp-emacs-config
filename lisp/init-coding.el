f7d9e282b39f8c15f56cb08ef81ec58d409ec3c2
liaotonglang <liaotonglang@gmail.com>
2022-04-11 14:01:32 +0800

    encoding to utf-8

--- lisp/init-coding.el
+++ lisp/init-coding.el
@@ -5,8 +5,21 @@
 ;; make UTF-8 the default coding system
 (when (fboundp 'set-charset-priority)
   (set-charset-priority 'unicode))
-(prefer-coding-system 'utf-8-unix)
-(setq locale-coding-system 'utf-8-unix)
+;; (prefer-coding-system 'utf-8-unix)
+;; (setq locale-coding-system 'utf-8-unix)
+
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(set-buffer-file-coding-system 'utf-8-unix)
+(set-clipboard-coding-system 'utf-8-unix)
+(set-file-name-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-next-selection-coding-system 'utf-8-unix)
+(set-selection-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(setq locale-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
 
 (provide 'init-coding)
 ;;; init-coding.el ends here
