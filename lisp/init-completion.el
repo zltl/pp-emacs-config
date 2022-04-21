d384fa03e765bb5f9973f9b60566a80b82b4b387
liaotonglang <liaotonglang@gmail.com>
2022-04-21 19:37:17 +0800

    enable yasnippet

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -40,8 +40,8 @@
         company-tooltip-limit 20
         company-show-quick-access t
         company-minimum-prefix-length 1)
-  ;; (setq company-backends
-  ;; 	(mapcar #'company-mode/backend-with-yas company-backends))
+  (setq company-backends
+        (mapcar #'company-mode/backend-with-yas company-backends))
   )
 
 (use-package company-box
