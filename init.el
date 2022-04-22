65374a83a95903dd328371759cba61e896e2add4
liaotonglang <liaotonglang@gmail.com>
2022-04-22 11:12:02 +0800

    increase gc threshold

--- init.el
+++ init.el
@@ -7,10 +7,10 @@
 
 ;; Minimize garbage collection during startup
 (setq gc-cons-threshold most-positive-fixnum)
-;; Lower threshold back to 8 MiB (default is 800kB)
+;; Lower threshold back to 32 MiB (default is 800kB)
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold (expt 2 23))))
+            (setq gc-cons-threshold (expt 2 25))))
 
 ;; add ./lisp folder into load-path, so we can split the configure files
 ;; into this directory.
