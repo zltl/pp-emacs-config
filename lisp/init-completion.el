f40ec9fc807a534e75d8590929344cd78a63a1eb
liaotonglang <liaotonglang@gmail.com>
2022-05-16 18:40:51 +0800

    get yasnippet back because completion format need yasnippet to apply

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -10,27 +10,27 @@
 (diminish 'ivy-mode)
 
 ;; too slow, get rid of it
-;; (use-package yasnippet
-;;   :hook
-;;   (prog-mode . yas-minor-mode-on)
-;;   (text-mode . yas-minor-mode-on)
-;;   (yas-minor-mode . (lambda () (diminish 'yas-minor-mode)))
-;;   ;; :config
-;;   ;; (yas-global-mode 1)
-;;   )
+(use-package yasnippet
+  :hook
+  (prog-mode . yas-minor-mode-on)
+  (text-mode . yas-minor-mode-on)
+  (yas-minor-mode . (lambda () (diminish 'yas-minor-mode)))
+  ;; :config
+  ;; (yas-global-mode 1)
+  )
 ;; (use-package yasnippet-snippets)
 
 ;; Add yasnippet support for all company backends
 ;; https://github.com/syl20bnr/spacemacs/pull/179
-;; (defvar company-mode/enable-yas t
-;;   "Enable yasnippet for all backends.")
-;; (defun company-mode/backend-with-yas (backend)
-;;   "If company-mode/enable-yas, put company-yasnippet into BACKEND."
-;;   (if (or (not company-mode/enable-yas)
-;; 	  (and (listp backend) (member 'company-yasnippet backend)))
-;;       backend
-;;     (append (if (consp backend) backend (list backend))
-;;             '(:with company-yasnippet))))
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+(defun company-mode/backend-with-yas (backend)
+  "If company-mode/enable-yas, put company-yasnippet into BACKEND."
+  (if (or (not company-mode/enable-yas)
+	  (and (listp backend) (member 'company-yasnippet backend)))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
 
 (use-package company
   :ensure t
@@ -42,8 +42,8 @@
         company-tooltip-limit 20
         company-show-quick-access t
         company-minimum-prefix-length 1)
-  ;; (setq company-backends
-  ;;       (mapcar #'company-mode/backend-with-yas company-backends))
+  (setq company-backends
+        (mapcar #'company-mode/backend-with-yas company-backends))
   )
 
 ;; (use-package company-box
