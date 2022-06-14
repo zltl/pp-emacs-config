2e5907bead93b3b123a8b87734e073fe33eb5d39
liaotonglang <liaotonglang@gmail.com>
2022-06-14 18:00:37 +0800

    use recommend config of treemacs on project README

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -3,28 +3,62 @@
 ;;; Code:
 
 (use-package treemacs
-  :commands (treemacs
-             treemacs-follow-mode
-             treemacs-filewatch-mode
-             treemacs-fringe-indicator-mode)
-  :bind (("<f7>" . treemacs)
-         ("<f6>" . treemacs-select-window))
+  :ensure t
+  :defer t
   :init
-  (setq treemacs-width 40
-        ;; treemacs-is-never-other-window t
-        ;; treemacs-indentation 0
-        ;; treemacs-space-between-root-nodes nil
-		)
-  (treemacs-follow-mode t)
-  (treemacs-project-follow-mode 1)
-  (treemacs-filewatch-mode t)
-  (treemacs-fringe-indicator-mode nil)
-  (other-window 1))
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-width 40)
+	;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+	(treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+		("C-x t p"   . treemacs-display-current-project-exclusively)
+        ;; ("C-x t C-t" . treemacs-find-file)
+        ;; ("C-x t M-t" . treemacs-find-tag)
+		))
 
 (use-package treemacs-projectile
-  :after (treemacs projectile))
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
 (use-package treemacs-magit
-  :after (treemacs magit))
+  :after (treemacs magit)
+  :ensure t)
+
+(use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
+
+(use-package treemacs-tab-bar ;;treemacs-tab-bar if you use tab-bar-mode
+  :after (treemacs)
+  :ensure t
+  :config (treemacs-set-scope-type 'Tabs))
 
 (provide 'init-treemacs)
 ;;; init-treemacs.el ends here
