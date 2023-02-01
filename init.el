d7f56bd47cc3d35a731ce103ba97ad5ed53b5698
liaotonglang <liaotonglang@gmail.com>
2023-02-01 15:20:31 +0800

    go fmt when save golang source code files

--- init.el
+++ init.el
@@ -164,6 +164,13 @@
 (add-hook 'go-mode-hook #'lsp)
 (add-hook 'rust-mode-hook #'lsp)
 
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
 ;; The Rust style guide recommends spaces rather than tabs for indentation
 (defun turn-off-indent-tabs-mode ()
   (setq indent-tabs-mode nil))
