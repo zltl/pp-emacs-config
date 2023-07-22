134a1f70dff4eb94eb8ba88bcc37fb2b96a734e5
liao tonglang <liaotonglang@gmail.com>
2023-07-23 02:02:43 +0800

    use emacs29 use-package build in to wrap config

--- init.el
+++ init.el
@@ -1151,3 +1151,21 @@ This uses `split-window-right' but follows with the cursor."
          ("C-c C-d" . #'helpful-at-point)
          ("C-h F" . #'helpful-function)
          ("C-h C" . #'helpful-command)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-vc-selected-packages
+   '((copilot :vc-backend Git :url
+              "https://github.com/zerolfx/copilot.el")
+     (on :vc-backend Git :url "https://github.com/ajgrf/on.el")
+     (gcmh :vc-backend Git :url "https://github.com/emacsmirror/gcmh")
+     (vc-use-package :vc-backend Git :url
+                     "https://github.com/slotThe/vc-use-package"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
